f4e3a9ff6a12e31374aced8ddba0deeb709ab027
Fbonilla15 <favio7155@gmail.com>
2023-11-21 16:02:10 -0500

    commit 1

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+function openModal(accion) {
+    if (accion == 'eliminarHotel') {
+        document.getElementById("modalEliminarHotel").style.display = "block";
+    } else if (accion == 'editarHotel' || accion == 'agregarHotel') {
+        document.getElementById("modalEditarHotel").style.display = "block";
+    } else if (accion == 'eliminarHabitacion') {
+        document.getElementById("modalEliminarHabitacion").style.display = "block";
+    } else if (accion == 'editarHabitacion' || accion == 'agregarHabitacion') {
+        document.getElementById("modalEditarHabitacion").style.display = "block";
+    }
+}
+
+// Función para cerrar la modal
+function closeModal(accion) {
+    if (accion == 'eliminarHotel') {
+        document.getElementById("modalEliminarHotel").style.display = "none";
+    } else if (accion == 'editarHotel' || accion == 'agregarHotel') {
+        document.getElementById("modalEditarHotel").style.display = "none";
+    }else if (accion == 'eliminarHabitacion') {
+        document.getElementById("modalEliminarHabitacion").style.display = "none";
+    } else if (accion == 'editarHabitacion' || accion == 'agregarHabitacion') {
+        document.getElementById("modalEditarHabitacion").style.display = "none";
+    }
+}
+
+function verGrafica(){
+    window.location.href = "./grafica.html";
+}
